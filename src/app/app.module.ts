e73d1989fd5936057f0832eac625f973fb2910fa
yaelbu <yael.buisine@gmail.com>
2021-09-14 23:24:18 +0300

    Routing Utilisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,42 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+
+
+const routes:Routes=[
+  {
+    path:'',
+    redirectTo:'post-list',
+    pathMatch:'full'
+  },
+  {
+    path:'post-list',
+    component:PostListComponent
+  },{
+    path:'auth',
+    component:AuthComponent
+  },
+  {
+    path:'post-add',
+    component:PostEditComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PostComponent,
-    PostListComponent
+    PostListComponent,
+    AuthComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
